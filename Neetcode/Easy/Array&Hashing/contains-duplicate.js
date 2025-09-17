1b79a3c0736b7b0c64031624e2bf4bf15529fb38
Vishesh Mehrotra <vishesh@kredx.com>
2025-09-17 18:56:40 +0530

    Solved - 217 - Contains duplicate

---
+++ Neetcode/Easy/Array&Hashing/contains-duplicate.js
@@ -0,0 +1,17 @@
+function containsDuplicate(nums){
+let set =new Set()
+for(let num of nums){
+    if(set.has(num))
+        return true;
+    else set.add(num);
+}
+return false
+}
+console.log(containsDuplicate([1,2,3,1]))
+console.log(containsDuplicate([1,2,3,4]))
+console.log(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+
+// output - 
+// true
+// false
+// true
