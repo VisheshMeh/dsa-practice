93530a342f71ad035ddf3c14f5a552c7e462b0f8
Vishesh Mehrotra <vishesh@kredx.com>
2025-09-18 09:23:51 +0530

    Solved - 242 - Valid Anagram

---
+++ Neetcode/Easy/Array&Hashing/anagram.js
@@ -0,0 +1,20 @@
+function anagram(s,t){
+    if(s.length !==t.length) return false 
+    const freq={};
+    for(let char of s){
+        freq[char]= (freq[char]||0) +1
+    }
+    for(let char of t){
+    if(!freq[char]) return false
+    freq[char]--
+    }
+    return true
+}
+console.log (anagram("anagram","nagaram"))
+console.log (anagram("rat","car"))
+console.log (anagram("batata","tataba"))
+
+// output: 
+// true
+// false
+// true
